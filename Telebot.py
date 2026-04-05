6bb7f412c09f163b20a7cf85035eca18d6f4d74e
Vitalik2828 <135163230+Vitalik2828@users.noreply.github.com>
2026-04-06 02:04:44 +0300

    Update and rename README.md to Telebot.py

---
+++ Telebot.py
@@ -0,0 +1,67 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    ApplicationBuilder,
+    MessageHandler,
+    CallbackQueryHandler,
+    ContextTypes,
+    filters,
+)
+import subprocess
+import os
+
+TOKEN = os.getenv(8659954382:AAFNXZxsc__jerB0C3LwuA4OhFZLDeATLPQ)
+ALLOWED_USER = int(os.getenv("1980197284"))
+
+# сохраняем ссылку
+USER_LINK = {}
+
+async def receive_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != ALLOWED_USER:
+        return
+
+    url = update.message.text
+    USER_LINK[update.effective_user.id] = url
+
+    keyboard = [
+        [
+            InlineKeyboardButton("📹 Видео", callback_data="video"),
+            InlineKeyboardButton("🎵 MP3", callback_data="mp3"),
+        ]
+    ]
+
+    await update.message.reply_text(
+        "Выбери формат:",
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
+
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    user_id = query.from_user.id
+
+    if user_id != ALLOWED_USER:
+        return
+
+    url = USER_LINK.get(user_id)
+
+    await query.edit_message_text("⬇️ Скачиваю...")
+
+    if query.data == "video":
+        subprocess.run(["yt-dlp", url])
+
+    elif query.data == "mp3":
+        subprocess.run(
+            ["yt-dlp", "-x", "--audio-format", "mp3", url]
+        )
+
+    await query.message.reply_text("✅ Готово!")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, receive_link))
+app.add_handler(CallbackQueryHandler(buttons))
+
+app.run_polling()
